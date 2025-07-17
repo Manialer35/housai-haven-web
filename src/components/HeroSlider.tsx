9fac6bf28ef7ed5fd3e3eead82723678e6160084
Manialer35 <84907731+Manialer35@users.noreply.github.com>
2025-07-17 16:40:57 +0530

    Update HeroSlider.tsx

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -62,7 +62,7 @@ const HeroSlider = () => {
             style={{ backgroundImage: `url(${slide.image})` }}
           >
             {/* Overlay */}
-            <div className="absolute inset-0 bg-gradient-to-r from-black/30 to-black/30 z-10"></div>
+            //<div className="absolute inset-0 bg-gradient-to-r from-black/30 to-black/30 z-10"></div>
             
             {/* Content */}
             <div className="relative z-10 h-full flex items-center">
