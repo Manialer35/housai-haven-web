987cf5f5cc6a85610816186d1feeb481eaee7dcf
Manialer35 <84907731+Manialer35@users.noreply.github.com>
2025-07-18 18:41:51 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom';
 import yoga from '@/assets/yoga.jpg';
 import enjoyment from '@/assets/enjoyment.jpg';
 import festivals from '@/assets/festivals1.jpg';
-import MusicalInstruments from '@/assets/games.webp';
-import Games from '@/assets/art.jpeg';
+import games from '@/assets/games.webp';
+import art from '@/assets/art.jpeg';
 import prayers from '@/assets/prayers.jpg';
 
 const HomePage = () => {
